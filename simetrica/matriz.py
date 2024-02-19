5f75d6aa13e67bc94de4279cdc54c13b702228da
LAZR <zluisangel03@gmail.com>
2024-02-18 20:14:16 -0600

    commit matriz

--- simetrica/matriz.py
+++ simetrica/matriz.py
@@ -1,5 +1,13 @@
 def es_simetrica(matriz):
     """
-    Porposito: retorna true o false segun si la martiz es simetrica
+    UWU hijos de puta me dejaron la mas dificil
+    esos bastardos me mintieron
     """
-    return False
+    if len(matriz) != len(matriz[0]):
+        return False
+    for i in range(len(matriz)-1):
+        for j in range(len(matriz[0])-1):
+            if matriz[i][j] != matriz[j][i]:
+                return False
+    return True
+
--- simetrica/test_matriz.py
+++ simetrica/test_matriz.py
@@ -3,12 +3,12 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "matriz,resultado",
+    "matris,resultado",
     [
         ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], False),
         ([[3, 2, 8], [2, 4, 0], [8, 0, 6]], True),
         ([[1, 1, 1], [2, 2, 2], [3, 3, 3]], False),
     ],
 )
-def test_matriz(matriz, resultado):
-    assert matriz.es_simetrica(matriz) == resultado
+def test_matriz(matris, resultado):
+    assert matriz.es_simetrica(matris) == resultado
